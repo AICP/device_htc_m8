84fd2b599d5f75894e46d53581ef7a6c6104a1d0
Bruno Martins <bgcngm@gmail.com>
2020-12-04 21:12:38 +0000

    m8: Inherit again from common_full_phone
    This reverts commit e16e70488fbc519f5076f3d72ccbcc74c8584929.
    
    Change-Id: I847888be5a8e5f25926ce19666f48cc39e9e2e26

--- aicp_m8.mk
+++ aicp_m8.mk
@@ -20,7 +20,7 @@
 $(call inherit-product, device/htc/m8/device.mk)
 
 # Inherit some common AICP stuff.
-$(call inherit-product, vendor/aicp/config/common_mini_phone.mk)
+$(call inherit-product, vendor/aicp/config/common_full_phone.mk)
 
 # Device identifier. This must come after all inclusions.
 PRODUCT_NAME := aicp_m8
